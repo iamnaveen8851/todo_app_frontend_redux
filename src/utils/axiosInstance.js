7b1715242874b4a319f35f3149c89d70e9384ab0
iamnaveen8851 <nk24528.nk81@gmail.com>
2025-01-02 14:41:05 +0530

    Dev url added.

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL:
     import.meta.env.VITE_NODE_ENV === "production"
-      ? import.meta.env.VITE_PRO_URL
+      ? import.meta.env.VITE_DEV_URL
       : "",
   // add content type application/json in case needed
   withCredentials: true, // Include cookies for cross-origin requests
