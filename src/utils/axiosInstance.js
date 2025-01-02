274fcde868c3ab2c9b15a5bb70b801990327b3eb
iamnaveen8851 <nk24528.nk81@gmail.com>
2025-01-02 11:38:15 +0530

    VITE_NODE_ENV updated

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   baseURL:
     import.meta.env.VITE_NODE_ENV === "production"
       ? import.meta.env.VITE_PRO_URL
-      : import.meta.env.VITE_DEV_URL,
+      : "",
   // add content type application/json in case needed
   withCredentials: true, // Include cookies for cross-origin requests
 });
